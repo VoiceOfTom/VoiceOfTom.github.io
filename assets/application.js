d0165f4c5e0cc465bc6e105f13949b0e2c67ae6e
VoiceOfTom <thomas@voiceoftom.com>
2016-08-22 23:53:42 +0200

    Delete application.js

--- assets/application.js
+++
@@ -1,9 +0,0 @@
-jQuery('a[href^="#"]').click(function(e) {
- 
-    jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
- 
-    return false;
- 
-    e.preventDefault();
- 
-});
